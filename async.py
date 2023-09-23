a5722926ec5867b6130106e28390b03a5e4a2ee2
amoshochman <amoshochman1982@gmail.com>
2023-09-23 15:42:37 +0300

    waiter task

--- async.py
+++ async.py
@@ -7,26 +7,45 @@ import asyncio
 from urls import sites
 import time
 
-saving_dict = defaultdict(int)
+
+class MyTask:
+    def __init__(self):
+        self.text = None
+        self.waiter_task = None
+        self.downloads = 0
+
+
+saving_dict = defaultdict(MyTask)
+
+
+async def waiter(event):
+    await event.wait()
 
 
 async def download(url, n):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            html = await response.text()
-            filename = unique_filename(url)
-            with open('data/' + filename, 'w') as file:
-                saving_dict[filename] += 1
-                file.write(html)
-            print("The requested char is " + html[n])
+        event = asyncio.Event()
+        filename = unique_filename(url)
+        if url not in saving_dict:
+            saving_dict[url].waiter_task = asyncio.create_task(waiter(event))
+            async with session.get(url) as response:
+                html = await response.text()
+                with open('data/' + filename, 'w') as file:
+                    file.write(html)
+                    saving_dict[filename].downloads += 1
+                event.set()
+                print("The requested char is " + html[n])
+        else:
+            await saving_dict[url].waiter_task
+            with open('data/' + filename) as file:
+                kk = file.read()
+                print("The requested char is " + kk[n])
 
 
 def unique_filename(url):
     url_without_protocol = url.replace('http://', '').replace('https://', '')
     sanitized_url = url_without_protocol.replace('/', '_').replace('.', '_')
-    filename = sanitized_url + '.html'
-
-    return filename
+    return sanitized_url + '.html'
 
 
 async def main():
@@ -40,5 +59,6 @@ if __name__ == '__main__':
     s = time.perf_counter()
     asyncio.run(main())
     elapsed = time.perf_counter() - s
-    print("max times a url was saved: " + str(max(saving_dict.values())))
+    downloads = (elem.downloads for elem in saving_dict.values())
+    print("max times a url was saved: " + str(max(downloads)))
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
