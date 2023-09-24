ff768bd110de64097886aa69d221a01d87665b02
amoshochman <amoshochman1982@gmail.com>
2023-09-24 10:52:51 +0300

    new folder

--- concurrent_downloads/main.py
+++ concurrent_downloads/main.py
@@ -1,8 +1,7 @@
 import my_async
-import my_sync
 import sys
 
 if __name__ == '__main__':
-    my_sync_results = my_sync.my_sync_main()
+    #my_sync_results = my_sync.my_sync_main()
     my_async_results = my_async.my_async_main(int(sys.argv[1]) if 1 < len(sys.argv) else None)
     #assert my_async_results == my_sync_results
