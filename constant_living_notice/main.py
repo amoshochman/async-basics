93686365d01ad0a920a09c68154dbadf989d6e38
amoshochman <amoshochman1982@gmail.com>
2023-09-26 16:36:02 +0300

    adding multiprocessing analog to threads

--- constant_living_notice/main.py
+++ constant_living_notice/main.py
@@ -1,24 +1,36 @@
+import multiprocessing
 import threading
 import time
 
 computation_is_done = False
 
-def my_computation(computation_done_event):
-    time.sleep(10)
+
+def heavy_computation(computation_done_event):
+    result = 0
+    for i in range(10 ** 9):
+        result += i
     computation_done_event.set()
 
-def print_working(computation_done_event):
+
+def constant_print_update(computation_done_event):
     while not computation_done_event.is_set():
         print("Still working")
         time.sleep(5)
 
+
+def my_test(package, entity):
+    print("testing " + str(entity))
+    computation_done_event = package.Event()
+    start = time.time()
+    r1 = entity(target=heavy_computation, args=(computation_done_event,))
+    r2 = entity(target=constant_print_update, args=(computation_done_event,))
+    r1.start()
+    r2.start()
+    r1.join()
+    r2.join()
+    print("time passed: " + str(round(time.time() - start, 2)))
+
+
 if __name__ == "__main__":
-    computation_done_event = threading.Event()
-
-    computation_thread = threading.Thread(target=my_computation, args=(computation_done_event,))
-    print_thread = threading.Thread(target=print_working, args=(computation_done_event,))
-
-    computation_thread.start()
-    print_thread.start()
-
-    computation_thread.join()
+    my_test(threading, threading.Thread)
+    my_test(multiprocessing, multiprocessing.Process)
